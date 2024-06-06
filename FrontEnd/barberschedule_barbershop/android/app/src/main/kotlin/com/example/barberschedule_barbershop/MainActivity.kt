786fde19426af39b233066a910eb58fee2d65ff6
IagoAntunes <iagoantunes.f@gmail.com>
2024-06-05 21:43:31 -0300

    first commit

---
+++ FrontEnd/barberschedule_barbershop/android/app/src/main/kotlin/com/example/barberschedule_barbershop/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.barberschedule_barbershop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ FrontEnd/barberschedule_client/android/app/src/main/kotlin/com/example/barberschedule_client/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.barberschedule_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()

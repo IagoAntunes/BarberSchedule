3b4ccd0a8290489564f868ad83b3a1d535cc4c63
IagoAntunes <iagoantunes.f@gmail.com>
2024-06-13 14:46:32 -0300

    feat: Add Colors, Fonts

---
+++ FrontEnd/barberschedule_design_system/lib/settings/utils/hex_to_color.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+abstract class HexToColor {
+  static Color toColor(String code) {
+    code = code.replaceAll('#', '');
+    return Color(int.parse(code, radix: 16) + 0xFF000000);
+  }
+}

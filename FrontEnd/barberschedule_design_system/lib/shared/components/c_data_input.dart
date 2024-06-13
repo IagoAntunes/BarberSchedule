7f793681ddb04aaddbd06541264698265b08a98a
IagoAntunes <iagoantunes.f@gmail.com>
2024-06-13 17:04:10 -0300

    feat: Components | Styles

---
+++ FrontEnd/barberschedule_design_system/lib/shared/components/c_data_input.dart
@@ -0,0 +1,96 @@
+import 'package:barberschedule_design_system/settings/style/app_style_colors.dart';
+import 'package:barberschedule_design_system/settings/style/app_text_style.dart';
+import 'package:flutter/material.dart';
+
+enum CCDataInputState {
+  primary,
+  active,
+}
+
+class CDataInput extends StatefulWidget {
+  const CDataInput({
+    super.key,
+    this.hintText,
+    required this.controller,
+  });
+  final String? hintText;
+  final TextEditingController controller;
+  @override
+  State<CDataInput> createState() => _CDataInputState();
+}
+
+class _CDataInputState extends State<CDataInput> {
+  CCDataInputState cFormFieldState = CCDataInputState.primary;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: widget.controller,
+      style: hintStyleByType(),
+      readOnly: true,
+      onTap: () async {
+        setState(() {
+          cFormFieldState = CCDataInputState.active;
+        });
+        await showDatePicker(
+          context: context,
+          initialDate: DateTime.now(),
+          firstDate: DateTime.now(),
+          lastDate:
+              DateTime.now().add(const Duration(seconds: 999999999999999999)),
+        ).then((value) {
+          setState(() {
+            cFormFieldState = CCDataInputState.primary;
+            if (value != null) widget.controller.text = value.toString();
+          });
+        });
+      },
+      decoration: InputDecoration(
+        hintText: widget.hintText,
+        hintStyle: hintStyleByType(),
+        prefixIcon: const Icon(Icons.date_range),
+        suffixIcon: Icon(
+          cFormFieldState == CCDataInputState.primary
+              ? Icons.keyboard_arrow_down
+              : Icons.keyboard_arrow_up,
+          color: AppStyleColors.gray300,
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(
+            color: AppStyleColors.yellowDark,
+            width: 1.0,
+          ),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(
+            color: AppStyleColors.gray500,
+            width: 1.0,
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(
+            color: AppStyleColors.yellowDark,
+            width: 1.0,
+          ),
+        ),
+      ),
+    );
+  }
+
+  TextStyle? hintStyleByType() {
+    switch (cFormFieldState) {
+      default:
+        return AppTextStyle.textMd.copyWith(
+          color: AppStyleColors.gray200,
+        );
+    }
+  }
+}
